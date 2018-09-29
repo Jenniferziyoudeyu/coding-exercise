717fed45948553e1467394904a27da01d3add6ff
Jenniferziyoudeyu <makyouki@gmail.com>
2018-09-29 16:56:05 -0700

    This is a mergeSort exercise.

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,21 @@ public class Main {
 //        for (int i = 0; i < thisIsReturnValue.length; i++) {
 //            System.out.print(thisIsReturnValue[i] + ",");
 //        }
-        QuickSort text = new QuickSort();
-        int[] Qsarray = {3,5,2,7,8,0,1};
-        int[] returnValue = text.quickSort(Qsarray);
-        for(int i = 0; i < returnValue.length; i++) {
-            System.out.print(returnValue[i] + ",");
+//        QuickSort testQuickSort = new QuickSort();
+  //      int[] Qsarray = {3,5,2,7,8,0,1};
+    //    int[] returnValue = testQuickSort.quickSort(Qsarray);
+//        for(int i = 0; i < returnValue.length; i++) {
+ //        System.out.print(returnValue[i] + ",");
+//        }
+    //    }
+
+        MergeSort testMergeSort = new MergeSort();
+        int[] mergeSortArray = {5,3,7,1,8,2};
+        int[] mergeSortReturnValue = testMergeSort.mergeSort(mergeSortArray);
+        for (int j = 0; j < mergeSortReturnValue.length; j++) {
+            System.out.print(mergeSortArray[j] + ",");
         }
+
 
 //        int num1 = 1, num2 = 1;
 //        int addResult = add(num1, num2);
---
+++ src/MergeSort.java
@@ -0,0 +1,46 @@
+public class MergeSort {
+    /**
+     * Given an array of integers, sort the elements in the array in ascending order. The merge sort algorithm should be used to solve this problem.
+     *
+     * Examples
+     *
+     * {1} is sorted to {1}
+     * {1, 2, 3} is sorted to {1, 2, 3}
+     * {3, 2, 1} is sorted to {1, 2, 3}
+     * {4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
+     * Corner Cases
+     *
+     * What if the given array is null? In this case, we do not need to do anything.
+     * What if the given array is of length zero? In this case, we do not need to do anything.
+     *
+     */
+
+    public int[] mergeSort(int[] array) {
+        mergeSortHelper(array, new int[array.length], 0, array.length - 1);
+        return array;
+    }
+
+    private void mergeSortHelper (int[] array, int[] helper, int start, int end) {
+        if (start >= end) return;
+        int mid = start + ( end - start)/2;
+        mergeSortHelper(array, helper, start, mid);
+        mergeSortHelper(array, helper, mid + 1, end);
+        int i = start; int j = mid + 1;
+        int helperIndex = start;
+        while (i <= mid || j <= end) {
+            int left = i <= mid ? array[i] : Integer.MAX_VALUE;
+            int right = j <= end? array[j] : Integer.MAX_VALUE;
+            if (left < right) {
+                helper[helperIndex] = left;
+                i++;
+            }else {
+                helper[helperIndex] = right;
+                j++;
+            }
+            helperIndex++;
+        }
+        for (int a = start; a <= end; a++){
+            array[a] = helper[a];
+        }
+    }
+}

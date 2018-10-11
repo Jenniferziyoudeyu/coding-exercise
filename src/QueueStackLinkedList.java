69feed3c73785a8c6d8487522f52da7c1242d2b2
Jenniferziyoudeyu <makyouki@gmail.com>
2018-10-11 11:58:08 -0700

    queue

---
+++ src/QueueStackLinkedList.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+
+public class QueueStackLinkedList {
+
+//    public ListNode reserve (ListNode head){
+//        if (head == null) {
+//            return head;
+//        }
+//        ListNode next = head.next;
+//        ListNode prev = null;
+//
+//        while (head.next != null) {
+//            next = head.next; //never lose the ownership of the head of a linkedList
+//            head.next = prev; // reverse the next pointer
+//            prev = head;
+//            head = next
+//        }
+//        return head;
+//    }
+//
+//}
+//
+//class ListNode {
+//    int val;
+//    ListNode head;
+//    ListNode next;
+//    void LinkedList(int val) {
+//        this.val =val;
+//    }
+}
---
+++ src/class3_Queue_Stack_LinkedList/QueueByTwoStacks.java
@@ -0,0 +1,64 @@
+package class3_Queue_Stack_LinkedList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.Stack;
+
+public class QueueByTwoStacks {
+    /**
+     * Description
+     * Java: Implement a queue by using two stacks. The queue should provide     size(), isEmpty(), offer(), poll() and peek() operations. When the queue is empty, poll() and peek() should return null.
+     *
+     * C++: Implement a queue by using two stacks. The queue should provide size(), isEmpty(), push(), front() and pop() operations. When the queue is empty, front() should return -1.
+     *
+     * Assumptions
+     *
+     * The elements in the queue are all Integers.
+     * size() should return the number of elements buffered in the queue.
+     * isEmpty() should return true if there is no element buffered in the queue, false otherwise.
+     */
+    public class Solution {
+
+        Stack<Integer> stack1;
+        Stack<Integer> stack2;
+
+        public Solution() {
+            this.stack1 = new Stack<>();
+            this.stack2 = new Stack<>();
+        }
+
+        public Integer poll() {
+            pushAllToStack2();
+            return stack2.isEmpty() ? 0 : stack2.pop();
+        }
+
+        public void offer(int element) {
+            stack1.push(element);
+        }
+
+        public Integer peek() {
+            pushAllToStack2();
+            return stack2.isEmpty() ? 0 : stack2.peek();
+        }
+
+        public int size() {
+            return stack1.size() + stack2.size();
+        }
+
+        public boolean isEmpty() {
+            return stack1.isEmpty() && stack2.isEmpty();
+        }
+
+        private void pushAllToStack2() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+        }
+
+    }
+
+}
+

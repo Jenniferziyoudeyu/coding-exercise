e6e10674e56b40e2a1e60b1440a405b3d85ef474
Jenniferziyoudeyu <makyouki@gmail.com>
2018-10-19 14:07:07 -0700

    ListNode

--- src/class3_Queue_Stack_LinkedList/ListNodeReverse.java
+++ src/class3_Queue_Stack_LinkedList/ListNodeReverse.java
@@ -1,24 +1,48 @@
 package class3_Queue_Stack_LinkedList;
 
 import javax.xml.soap.Node;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class ListNodeReverse {
     //recursion 方法
-    public Node ReverseList (ListNode head) {
+    public ListNode ReverseList(ListNode head) {
+
         if (head == null || head.next == null) {
-            return null;
+            return head;
         }
-        if(head != null && head.next != null && head.next.next == null) {
-            head.next.next = head;
+        ListNode next = head.next;
+        ListNode prev = null;
+        while (head != null) {
+            next = head.next; //永远别丢了正方向linkedlist的头
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        ListNode newhead = reverse(next);
+        next.next = head;
+        head.next = null;
+        return newhead;
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode prev;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
         }
     }
 }
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode (int val) {
-        this.val = val;
-    }
-}

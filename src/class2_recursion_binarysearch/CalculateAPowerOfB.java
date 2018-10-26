ca32383a8c7d83c8a34f3ddd68e54c9d814e256b
Jenniferziyoudeyu <makyouki@gmail.com>
2018-10-26 12:37:39 -0700

    changes

--- src/class2_recursion_binarysearch/CalculateAPowerOfB.java
+++ src/class2_recursion_binarysearch/CalculateAPowerOfB.java
@@ -1,5 +1,28 @@
 package class2_recursion_binarysearch;
-
+/**
+ * Description
+ * Evaluate a to the power of b, assuming both a and b are integers and b is non-negative.
+ *
+ * Examples
+ *
+ * power(2, 0) = 1
+ * power(2, 3) = 8
+ * power(0, 10) = 0
+ * power(-2, 5) = -32
+ * Corner Cases
+ *
+ * What if the result is overflowed? We can assume the result will not be overflowed when we solve this problem on this online judge.
+ *               a^7
+ *            /     \
+ *         a^4       a^3
+ *        /  \      /  \
+ *     a^2  a^2   a^2   a
+ *     /\   /\    /\   /
+ *    a a  a a   a a  1
+ *   / /  / /   / /
+ *  1 1  1 1   1 1
+ *
+ */
 /*
 时间复杂度
 b = n,

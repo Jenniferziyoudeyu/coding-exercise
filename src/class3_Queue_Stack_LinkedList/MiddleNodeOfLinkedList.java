05c785a4cbac545087ce6bfe73f321e7eb72ea82
Jenniferziyoudeyu <makyouki@gmail.com>
2018-10-26 13:59:51 -0700

    middle node of LinkedList

---
+++ src/class3_Queue_Stack_LinkedList/MiddleNodeOfLinkedList.java
@@ -0,0 +1,28 @@
+package class3_Queue_Stack_LinkedList;
+
+import java.util.List;
+
+/**
+ * Description
+ * Find the middle node of a given linked list.
+ *
+ * Examples
+ *
+ * L = null, return null
+ * L = 1 -> null, return 1
+ * L = 1 -> 2 -> null, return 1
+ * L = 1 -> 2 -> 3 -> null, return 2
+ * L = 1 -> 2 -> 3 -> 4 -> null, return 2
+ *
+ */
+public class MiddleNodeOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}

770830452958ae5a8335f3e13d7a8a03291592f3
Jenniferziyoudeyu <makyouki@gmail.com>
2019-02-01 14:49:49 -0800

    Elevator class

---
+++ src/testAndPractice/Elevator.java
@@ -0,0 +1,65 @@
+package testAndPractice;
+
+import java.util.List;
+
+public class Elevator {
+    public Status status;
+    public Direction direction;
+    private int maxWeight;
+    private int curWeight;
+
+    public Elevator(Status myStatus, Direction myDirection, int maxWeight) {
+        status = myStatus;
+//        myStatus = Status.open;
+        direction = myDirection;
+        this.maxWeight = maxWeight;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    public Status getStatus() {
+        return status;
+    }
+
+    public void st() {
+        if (status == Status.open) {
+            status = Status.close;
+        }
+
+
+        Elevator elevator = new Elevator(Status.open, Direction.up, 3000);
+        elevator.st();
+//            elevator.setStatus();
+    }
+
+    public int addWeight(int inputWeight) {
+        return curWeight += inputWeight;
+    }
+
+    public int minusWeight(int inputWeight) {
+        return curWeight -= inputWeight;
+    }
+
+    private boolean isOverweight() {
+        return curWeight > maxWeight;
+    }
+
+    public Direction whereToGo(int curLevel, int targetLevel) {
+        if (curLevel == targetLevel) return Direction.stay;
+        else if (curLevel > targetLevel) return Direction.down;
+        else return Direction.up;
+
+    }
+}
+
+enum Status {
+    open,
+    close
+}
+
+enum Direction {
+    up,
+    down,
+    stay
+}
---
+++ src/testAndPractice/Elevetor.java
@@ -0,0 +1,32 @@
+package testAndPractice;
+
+public class Elevetor {
+    public boolean status;
+    public float maxWeight;
+    private int level;
+
+    public Elevetor(boolean doorOpenStatus, boolean goUp, int speed, int maxWeight) {
+        this.level = 0;
+    }
+
+    public boolean overWeight(int weight) {
+        int numOfPeople = 0;
+        Elevetor elevetor = new Elevetor(false, true, 2, 1000);
+        while (numOfPeople * weight < elevetor.maxWeight) {
+            numOfPeople += 1;
+        }
+        return true;
+    }
+}
+
+class a extends Elevetor {
+    public a(boolean doorOpenStatus, boolean goUp, int speed, int maxWeight) {
+        super(doorOpenStatus, goUp, speed, maxWeight);
+    }
+
+    @Override
+    public boolean overWeight(int weight) {
+       boolean superRes = super.overWeight(weight);
+        return !superRes;
+    }
+}
